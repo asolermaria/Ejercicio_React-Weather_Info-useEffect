6fff407a2b32f6c1a77c6af3f726cd2d9dcf23e0
asolermaria <asolermaria@gmail.com>
2026-05-05 20:19:46 +0200

    Done forecast search by user browser geolocation, add index.css & README.md

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,24 @@ function App() {
 
   const apiKey = import.meta.env.VITE_API_KEY;
 
+  // Sólo se ejecuta una vez al cargar la página, intenta buscar prnóstico por la geolocalización del navegador
   useEffect(() => {
-    // Se ejecuta cada vez que cambia el valor de city (submit del Form)
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        const lat = pos.coords.latitude;
+        const lon = pos.coords.longitude;
+
+        fetchWeatherCoords(lat, lon);
+      },
+      (error) => {
+        console.log(error);
+        fetchWeather("Madrid");
+      },
+    );
+  }, []);
+
+  // Se ejecuta cada vez que cambia el valor de city (submit del Form)
+  useEffect(() => {
     fetchWeather(city);
   }, [city]);
 
@@ -23,19 +39,49 @@ function App() {
 
       const data = await res.json();
 
-      if (data.cod === "404") {
+      if (data.cod !== "200" && data.cod !== 200) {
+        // Si la respuesta no es exitosa (distinto de 200)
         setError("Ciudad no encontrada");
         setWeather([]);
+        setCity("");
         return;
       }
 
+      // Filtramos los datos para quedarnos con el pronóstico de las 12:00 de cada día, así sólo tendremos un registro por día
       const onePerDay = data.list.filter((item) =>
         item.dt_txt.includes("12:00:00"),
       );
 
       setError("");
       setWeather(onePerDay);
-      console.log(weather);
+      // console.log(weather);
+    } catch (error) {
+      setError("Error de conexión");
+    }
+  };
+
+  const fetchWeatherCoords = async (lat, lon) => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=es`,
+      );
+
+      const data = await res.json();
+
+      if (data.cod !== "200" && data.cod !== 200) {
+        // Si la respuesta no es exitosa (distinto de 200)
+        setError("No se pudo obtener el clima");
+        return;
+      }
+
+      // Filtramos los datos para quedarnos con el pronóstico de las 12:00 de cada día, así sólo tendremos un registro por día
+      const onePerDay = data.list.filter((item) =>
+        item.dt_txt.includes("12:00:00"),
+      );
+
+      setCity(data.city.name);
+      setWeather(onePerDay);
+      setError("");
     } catch (error) {
       setError("Error de conexión");
     }
@@ -46,6 +92,7 @@ function App() {
       <h1>Weather App</h1>
       <Form setCity={setCity} />
       {error && <p>{error}</p>}
+      {city !== "" && <h2>Pronóstico de {city}</h2>}
       <WeatherList weather={weather} />
     </main>
   );
